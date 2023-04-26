02ce354546adff427d8a3feb31b59a1304f2f464
korecbtc <99734892+korecbtc@users.noreply.github.com>
2023-04-26 15:21:53 +0300

    allure steps added

--- test_OSM.py
+++ test_OSM.py
@@ -2,14 +2,16 @@ from requests import get, ConnectionError
 import pytest
 import csv
 import logging
+import allure
 
-TESTED_URL = "https://nominatim.openstreetmap.org/search?format=json"
+TESTED_URL = "https://nominatim.openstreetmap.org/search"
 TESTED_URL_REVERSE = "https://nominatim.openstreetmap.org/reverse"
 ROUND_LEVEL = 3
 logger = logging.getLogger(__name__)
 
 
 def get_data(file):
+    '''Чтение входных данных из файла'''
     with open(file, 'r') as f:
         reader = csv.reader(f)
         next(reader)
@@ -22,24 +24,38 @@ data_structured = get_data('search_data_structured.csv')
 data_reverse = get_data('reverse_data.csv')
 
 
+@allure.step('Отправка запроса на сервер')
+def connection(tested_url, params):
+    '''Отправка запроса на сервер'''
+    response = get(tested_url, params=params)
+    return response
+
+
+@allure.title("Search_by_q_param")
 @pytest.mark.parametrize('data', data)
 def test_search_by_q(data):
     '''Проверка эндпоинта search с параметром поиска в свободной форме'''
+    params = {
+                'q': data[0],
+                'format': 'json'
+             }
     try:
-        response = get(TESTED_URL, params={
-            'q': data[0],
-            'format': 'json'
-            })
+        response = connection(TESTED_URL, params)
     except ConnectionError as error:
         logger.error(error)
-    assert round(
-        float(response.json()[0]['lat']), ROUND_LEVEL
-        ) == round(float(data[1]), ROUND_LEVEL)
-    assert round(
-        float(response.json()[0]['lon']), ROUND_LEVEL
-        ) == round(float(data[2]), ROUND_LEVEL)
+    else:
+        logger.info('Соединение установлено')
+    with allure.step("Проверяю, что широта соответствует ожидаемой"):
+        assert round(
+            float(response.json()[0]['lat']), ROUND_LEVEL
+            ) == round(float(data[1]), ROUND_LEVEL)
+    with allure.step("Проверяю, что долгота соответствует ожидаемой"):
+        assert round(
+            float(response.json()[0]['lon']), ROUND_LEVEL
+            ) == round(float(data[2]), ROUND_LEVEL)
 
 
+@allure.title("Search_by_structured_params")
 @pytest.mark.parametrize('data_structured', data_structured)
 def test_search_by_params(data_structured):
     '''Проверка эндпоинта search со структурировынным запросом'''
@@ -53,17 +69,22 @@ def test_search_by_params(data_structured):
         'format': 'json'
     }
     try:
-        response = get(TESTED_URL, params=params)
+        response = connection(TESTED_URL, params)
     except ConnectionError as error:
         logger.error(error)
-    assert round(
-        float(response.json()[0]['lat']), ROUND_LEVEL
-        ) == round(float(data_structured[6]), ROUND_LEVEL)
-    assert round(
-        float(response.json()[0]['lon']), ROUND_LEVEL
-        ) == round(float(data_structured[7]), ROUND_LEVEL)
+    else:
+        logger.info('Соединение установлено')
+    with allure.step("Проверяю, что широта соответствует ожидаемой"):
+        assert round(
+            float(response.json()[0]['lat']), ROUND_LEVEL
+            ) == round(float(data_structured[6]), ROUND_LEVEL)
+    with allure.step("Проверяю, что долгота соответствует ожидаемой"):
+        assert round(
+            float(response.json()[0]['lon']), ROUND_LEVEL
+            ) == round(float(data_structured[7]), ROUND_LEVEL)
 
 
+@allure.title("Reverse_search")
 @pytest.mark.parametrize('data_reverse', data_reverse)
 def test_reverse(data_reverse):
     '''Проверка эндпоинта reverse'''
@@ -74,30 +95,32 @@ def test_reverse(data_reverse):
         'details': 1
     }
     try:
-        response = get(TESTED_URL_REVERSE, params=params)
+        response = connection(TESTED_URL_REVERSE, params)
     except ConnectionError as error:
         logger.error(error)
+    else:
+        logger.info('Соединение установлено')
     if 'house_number' in response.json()['address']:
-        assert data_reverse[0] == response.json()['address']['house_number']
+        with allure.step("Проверяю, что номер дома соответствует ожидаемому"):
+            assert data_reverse[0] == (
+                response.json()['address']['house_number']
+            )
     if 'road' in response.json()['address']:
-        assert data_reverse[1].lower() in (
-            response.json()['address']['road'].lower()
-        )
+        with allure.step("Проверяю, что улица соответствует ожидаемой"):
+            assert data_reverse[1].lower() in (
+                response.json()['address']['road'].lower()
+            )
     if 'city' in response.json()['address']:
-        assert data_reverse[2].lower() in (
-            response.json()['address']['city'].lower()
+        with allure.step("Проверяю, что город соответствует ожидаемому"):
+            assert data_reverse[2].lower() in (
+                response.json()['address']['city'].lower()
+            )
+    elif 'town' in response.json()['address']:
+        with allure.step("Проверяю, что номер дома соответствует ожидаемому"):
+            assert data_reverse[2].lower() in (
+                response.json()['address']['town'].lower()
+            )
+    with allure.step("Проверяю, что страна соответствует ожидаемой"):
+        assert data_reverse[3].lower() in (
+            response.json()['address']['country'].lower()
         )
-    assert data_reverse[3].lower() in (
-        response.json()['address']['country'].lower()
-    )
-
-
-# for i in data_reverse:
-#     params = {
-#         'lat': i[4],
-#         'lon': i[5],
-#         'format': 'json',
-#         'details': 1
-#     }
-#     response = get(TESTED_URL_REVERSE, params=params)
-#     print(response.json()['address'])
